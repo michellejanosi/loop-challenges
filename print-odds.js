116251a08cac72137f531842cad08bf57e6a716c
michellejanosi <michelle.janosi@gmail.com>
2023-06-24 14:07:36 -0400

    Initial commit and add solution

---
+++ print-odds.js
@@ -0,0 +1,13 @@
+// Using a loop, write code that will console.log all of the odd values from 1 up to 20.
+
+// using a for loop
+for (let i = 1; i < 20; i+=2) {
+  console.log(i)
+}
+
+// using a while loop
+let i = 1;
+while (i < 20) {
+  console.log(i);
+  i += 2;
+}
